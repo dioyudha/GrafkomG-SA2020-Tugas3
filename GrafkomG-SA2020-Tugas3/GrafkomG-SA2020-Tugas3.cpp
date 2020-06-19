2fd95f79b9a186c338b08a6e92153194148d67d1
unknown <672018165@student.uksw.edu>
2020-06-19 18:38:05 +0700

    update sementara

--- GrafkomG-SA2020-Tugas3/GrafkomG-SA2020-Tugas3.cpp
+++ GrafkomG-SA2020-Tugas3/GrafkomG-SA2020-Tugas3.cpp
@@ -1,19 +1,51 @@
-#include <windows.h>
+#include <iostream>
 #include <GL/glut.h>
 
-void init(void);
-void tampil(void);
-void keybaord(unsigned char, int, int);
-void ukuran(int, int);
+#define ub unsigned char
 
-int is_depth;
-
-int main(int argc, char** argv)
+void render(void)
 {
+
+
+}
+
+void keyboardDown(ub key, int mouseX, int mouseY)
+{
+	system("cls");
+	std::cout << key << " " << std::endl;
+}
+void keyboardUp(ub key,int state, int mouseX, int mouseY)
+{
+	system("cls");
+	std::cout << key << state << std::endl;
+}
+void mouseMovement(int x, int y) {
+	system("cls");
+	std::cout << "mouseX:" << x << " | " << "mouseY:" << y << std::endl;
+}
+void mouse(int xbutton, int ystate, int x, int y) {
+	system("cls");
+	std::cout << "mouseX:" << x << " " << y << "mouseY:" << std::endl;
+}
+
+int main(int argc, char** argv) {
 	glutInit(&argc, argv);
-	glutInitDisplayMode();
-	glutInitWindowSize();
-	glutInitWindowPosition(250, 80);
-	glutCreateWindow("Dio Yudha Perdana 672018165");
-	init();
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_MULTISAMPLE);
+	glutInitWindowSize(100, 100);
+	glutInitWindowPosition(600, 300);
+	glutCreateWindow(argv[0]);
+	glutDisplayFunc(render);
+	glutMotionFunc(mouseMovement);
+	glutMouseFunc(mouse);
+	glutKeyboardFunc(keyboardDown);
+	glutKeyboardFunc(keyboardUp);
+
+
+	glClearColor(0, 0, 0, 1);
+	gluOrtho2D(0, 100, 100, 0);
+
+	glutMainLoop();
+	return 0;
+
+
 }
