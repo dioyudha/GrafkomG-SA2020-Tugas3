b9516408f2c454f00f778b8a50ae30bfa8535cbf
unknown <672018165@student.uksw.edu>
2020-06-24 15:41:16 +0700

    update siang

--- GrafkomG-SA2020-Tugas3/GrafkomG-SA2020-Tugas3.cpp
+++ GrafkomG-SA2020-Tugas3/GrafkomG-SA2020-Tugas3.cpp
@@ -1,50 +1,59 @@
 #include <iostream>
 #include <GL/glut.h>
 
-#define ub unsigned char
+unsigned char array;
+int index;
 
 void render(void)
 {
 
+}
+void keyboardDown(unsigned char key, int X, int Y)
+{
+	
+	std::cout << key;
+	key = array;
+	std::cout << array;
+	glutPostRedisplay();
 
 }
-
-void keyboardDown(ub key, int mouseX, int mouseY)
+void keyboardUp(unsigned char key, int X, int Y)
 {
+	
+	std::cout << key << std::endl;
+	glutPostRedisplay();
 	system("cls");
-	std::cout << key << " " << std::endl;
-}
-void keyboardUp(ub key, int mouseX, int mouseY)
-{
-	std::cout << key << " " << std::endl;
+
 }
 void mouseMovement(int x, int y) {
 	system("cls");
-	std::cout << "mouseX:" << x << " | " << "mouseY:" << y << std::endl;
+	std::cout << "mouseX: " << x << " | " << "mouseY: " << y << std::endl;
+	glutPostRedisplay();
 }
-void mouse(int xbutton, int ystate, int x, int y) {
+void myMouse(int button, int state, int x, int y) {
 	system("cls");
 	std::cout << "mouseX:" << x << " | " << "mouseY:" << y << std::endl;
+	glutPostRedisplay();
 }
+
 
 int main(int argc, char** argv) {
 	glutInit(&argc, argv);
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_MULTISAMPLE);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_MULTISAMPLE); 
 	glutInitWindowSize(100, 100);
 	glutInitWindowPosition(600, 300);
 	glutCreateWindow(argv[0]);
 	glutDisplayFunc(render);
-	glutMotionFunc(mouseMovement);
-	glutMouseFunc(mouse);
-	glutKeyboardFunc(keyboardDown);
-	glutKeyboardFunc(keyboardUp);
 
+	glutMotionFunc(mouseMovement);
+	glutMouseFunc(myMouse);
+	glutKeyboardFunc(keyboardDown);
+	glutKeyboardUpFunc(keyboardUp);
 
 	glClearColor(0, 0, 0, 1);
 	gluOrtho2D(0, 100, 100, 0);
 
 	glutMainLoop();
 	return 0;
-
 
 }
